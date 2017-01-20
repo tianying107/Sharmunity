a23d46c39ed08e448e9780f9c6986bc1008b846e
tianying107 <starfrombeijing@gmail.com>
2017-01-19 23:06:12 -0700

    add signup email

---
+++ Sharmunity/Classes/SYSignUpEmail.h
@@ -0,0 +1,13 @@
+//
+//  SYSignUpEmail.h
+//  Sharmunity
+//
+//  Created by st chen on 2017/1/19.
+//  Copyright © 2017年 Sharmunity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYSignUpEmail : UIView
+
+@end
---
+++ Sharmunity/Classes/SYSignUpEmail.m
@@ -0,0 +1,21 @@
+//
+//  SYSignUpEmail.m
+//  Sharmunity
+//
+//  Created by st chen on 2017/1/19.
+//  Copyright © 2017年 Sharmunity. All rights reserved.
+//
+
+#import "SYSignUpEmail.h"
+
+@implementation SYSignUpEmail
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
